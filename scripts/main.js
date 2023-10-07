403ffdada23f8635c6832bb1f17c1e7aed70c258
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2023-10-07 09:48:00 +0200

    adding scroll function

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 
-  
+   
     let list = document.querySelector('.slider .list');
     let items = document.querySelectorAll('.slider .list .item');
     let dots = document.querySelectorAll('.slider .dots li');
     let prev = document.getElementById('prev');
     let next = document.getElementById('next');
    
-  
     let active = 0;
-    let lengthItems = items.length - 1;
-  
+    let lengthItems = items.length -1 ;
+
+
     next.onclick = function () {
       if (active + 1 > lengthItems) {
         active = 0;
@@ -26,21 +26,26 @@
  }
     reloadSlider();
 }
-   
 
-  function reloadSlider(){
-    let checkLeft = items[active].offsetLeft;
-    list.style.left = - checkLeft +'px';
-  
+function reloadSlider(){
+let checkLeft = items[active].offsetLeft;
+list.style.left = - checkLeft +'px';}
+
+
   let lastActiveDot = document.querySelector('.slider .dots li.active');
   lastActiveDot.classList.remove('.active');
+  dots [active].classList.add('active');
  
-}
+
 
 dots.forEach((li, key)=> {
-  li.addEventListener('click', function(){
+  li.addEventListener('click', function() {
     active = key;
     reloadSlider();
 
+ })
 })
-})
+
+
+
+
