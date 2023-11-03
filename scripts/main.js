c150533ffc0820e06db9a94bfa8351aca2402d97
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2023-11-03 15:42:06 +0100

    adding function

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,62 @@
 
-   
-    let list = document.querySelector('.slider .list');
-    let items = document.querySelectorAll('.slider .list .item');
-    let dots = document.querySelectorAll('.slider .dots li');
-    let prev = document.getElementById('prev');
-    let next = document.getElementById('next');
-   
-    let active = 0;
-    let lengthItems = items.length -1 ;
 
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
 
-    next.onclick = function () {
-      if (active + 1 > lengthItems) {
-        active = 0;
-      } else {
-        active = active + 1;
-   }
-      reloadSlider();
-  }
-    prev.onclick = function () {
-    if (active - 1 < 0 ) {
-      active = lengthItems;
-    } else {
-      active = active - 1;
- }
-    reloadSlider();
+let active = 0;
+let lengthItems = items.length - 1;
+
+function setActiveDot(index) {
+    document.querySelector('.dots li.active').classList.remove('active');
+    // Add the 'active' class to the clicked dot
+    dots[index].classList.add('active');
 }
 
-function reloadSlider(){
-let checkLeft = items[active].offsetLeft;
-list.style.left = - checkLeft +'px';}
-
-
-  let lastActiveDot = document.querySelector('.slider .dots li.active');
-  lastActiveDot.classList.remove('.active');
-  dots [active].classList.add('active');
- 
-
-
-dots.forEach((li, key)=> {
-  li.addEventListener('click', function() {
-    active = key;
+next.onclick = function () {
+    if (active + 1 > lengthItems) {
+        active = 0;
+    } else {
+        active = active + 1;
+    }
     reloadSlider();
+    setActiveDot(active);
+};
 
- })
-})
+prev.onclick = function () {
+    if (active - 1 < 0) {
+        active = lengthItems;
+    } else {
+        active = active - 1;
+    }
+    reloadSlider();
+    setActiveDot(active);
+};
 
+function reloadSlider() {
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+}
 
+dots.forEach((li, key) => {
+    li.addEventListener('click', function () {
+        active = key;
+        reloadSlider();
+        setActiveDot(active);
+    });
+});
 
-
+items.forEach((item) => {
+    item.addEventListener('click', function () {
+        if (active + 1 > lengthItems) {
+            active = 0;
+        } else {
+            active = active + 1;
+        }
+        console.log(active);
+        reloadSlider();
+        setActiveDot(active);
+    });
+});
